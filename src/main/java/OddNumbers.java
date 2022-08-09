510ff02e9e03413c2a4baff7a5b16cee51376d27
Nitish Bhamidipati <nitishbhamidipati@gmail.com>
2022-08-08 20:05:41 -0700

    u nade acie

---
+++ src/main/java/OddNumbers.java
@@ -0,0 +1,14 @@
+public class OddNumbers {
+    int[] nums = {1,2,3,4,5,6,7,8,9,10};
+
+    public void checkNums(){
+        //write a for loop that goes through all the elements of the nums array
+        //inside of the loop, use an if statement to check if the current number being checked is odd or even
+        //print the number ONLY if the number is odd
+    }
+
+    public static void main (String[] args){
+
+    }
+
+}
